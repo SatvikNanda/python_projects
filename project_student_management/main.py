a824b54b73d625faf64b5e0ead53c250d1b75de5
satvik nanda <satvik.nanda@gmail.com>
2024-11-13 15:52:35 +0530

    add_student functionality created

--- project_student_management/main.py
+++ project_student_management/main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
 import sys
 from PyQt6.QtGui import QAction
 import sqlite3
@@ -16,6 +16,8 @@ class MainWindow(QMainWindow):
 
         #add sub-menu for above menues
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
+
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -40,13 +42,69 @@ class MainWindow(QMainWindow):
 
         connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedHeight(500)
+        self.setFixedWidth(500) #this is a good practice for pop-up windows
+
+        layout = QVBoxLayout()
+
+        # add studentname widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # add combo box of courses
+        self.course_name = QComboBox()
+        courses = ["Biology", "Maths", "Physics", "Astronomy"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        #Add mobile widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        #add submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+
+        self.setLayout(layout)
+    
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+
+        connection = sqlite3.connect("C:/Users/satvi/OneDrive/Desktop/python_projects/project_student_management/database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name,course,mobile) VALUES (?,?,?)", (name,course,mobile))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+
+
+
+
 
 
 
 
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
-age_calculator.load_data()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
