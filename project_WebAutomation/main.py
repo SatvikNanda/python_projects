bfadf8a8e4cf4b7645597b0753f942f31de2f1a2
satvik nanda <satvik.nanda@gmail.com>
2025-01-03 16:04:23 +0530

    filled text boxes

--- project_WebAutomation/main.py
+++ project_WebAutomation/main.py
@@ -30,8 +30,34 @@ username_field.send_keys('pythonusername')
 password_field.send_keys('Pythonstudent123$') # To automate the process of filling the login fields
 
 # Find the login button and click it
-login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'login')))
-login_button.click()
+login_button = driver.find_element(By.ID, 'login')
+driver.execute_script("arguments[0].click()", login_button)
+
+
+# After logging in
+
+# Locate the Elements dropdown and Text Box
+elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div')))
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
+# Locate the form fields and submit button
+fullName_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+currentAddress_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
+permanentAddress_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+
+submit_button = driver.find_element(By.ID, 'submit')
+
+# Fill in the located fields
+fullName_field.send_keys('Satvik Nanda')
+email_field.send_keys("xyz@gmail.com")
+currentAddress_field.send_keys("street 121, xyz road, New York")
+permanentAddress_field.send_keys("street 121, xyz road, New York")
+
+driver.execute_script("arguments[0].click()", submit_button)
 
 
 
