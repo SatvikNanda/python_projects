9ad32aea86eede05aa2ac86648049cce3a1ab80e
satvik nanda <satvik.nanda@gmail.com>
2025-01-28 15:30:51 +0530

    changes

--- project-flask_form/app.py
+++ project-flask_form/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 # Handling http request
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
+        print(first_name, last_name, email, date, occupation)
+
+
+
     return render_template("index.html") #render template calls the index.html file
 
 
