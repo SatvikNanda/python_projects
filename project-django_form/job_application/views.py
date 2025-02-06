08552558c183a24f55899462bed70b3cfe086118
satvik nanda <satvik.nanda@gmail.com>
2025-02-06 13:34:45 +0530

    changes

--- project-django_form/job_application/views.py
+++ project-django_form/job_application/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm()
+        
+        first_name = form["first_name"]
+        last_name = form["last_name"]
+        email = form["email"]
+        date = form["date"]
+        occupation = form["occupation"]
+            
+        Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date, occupation=occupation)
+
+        messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
